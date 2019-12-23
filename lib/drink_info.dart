ef4c13034d0ceaf2d51c9e54ebbfb988274ebfa1
Matthew Whitaker <matthew.whitaker@arm.com>
2019-12-23 14:26:23 +0000

    Initial Commit v2

---
+++ lib/drink_info.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the drink.
+  final Drink drink;
+
+  // In the constructor, require a drink.
+  DetailScreen({Key key, @required this.drink}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the drink to create the UI.
+    final drinkPrice = Container(
+      padding: const EdgeInsets.all(7.0),
+      decoration: new BoxDecoration(
+          border: new Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(5.0)),
+      child: new Text(
+        "\£" + drink.price.toString(),
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+
+    final topContentText = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 120.0),
+        Icon(
+          Icons.local_drink,
+          color: Colors.white,
+          size: 40.0,
+        ),
+        Container(
+          width: 90.0,
+          child: new Divider(color: Colors.green),
+        ),
+        SizedBox(height: 10.0),
+        Text(
+          drink.name,
+          style: TextStyle(color: Colors.white, fontSize: 45.0),
+        ),
+        SizedBox(height: 30.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+//            Expanded(flex: 1, child: levelIndicator),
+            Expanded(
+                flex: 9,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      drink.desc,
+                      style: TextStyle(color: Colors.white),
+                    ))),
+            Expanded(flex: 2, child: drinkPrice)
+          ],
+        ),
+      ],
+    );
+
+    final topContent = Stack(
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.only(left: 10.0),
+            height: MediaQuery.of(context).size.height * 0.5,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new NetworkImage(drink.img),
+                fit: BoxFit.cover,
+              ),
+            )),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          padding: EdgeInsets.all(40.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+          child: Center(
+            child: topContentText,
+          ),
+        ),
+        Positioned(
+          left: 8.0,
+          top: 60.0,
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        )
+      ],
+    );
+
+    final bottomContentText = Text(
+      drink.info,
+      style: TextStyle(fontSize: 18.0,
+      color: Colors.white,
+      ),
+    );
+    final readButton = Container(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        width: MediaQuery.of(context).size.width,
+        child: RaisedButton(
+          onPressed: () => {},
+          color: Color.fromRGBO(58, 66, 86, 1.0),
+          child:
+          Text("ORDER", style: TextStyle(color: Colors.white)),
+        ));
+    final bottomContent = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(40.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[bottomContentText, readButton],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[topContent, bottomContent],
+        ),
+      ),
+    );
+  }
+}
